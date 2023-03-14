c43cfb6df602130ee20d831a42fe45411f0dd7dc
shunqor <example@gmail.com>
2023-03-14 17:28:25 +0300

    first commit

---
+++ JS/Script.js
@@ -0,0 +1,77 @@
+ "use strict";
+
+ //Number
+//  let number = 5.6; // data type number 
+//  console.log(-8 / 0) // infinitive
+//  console.log("Shunqor" * 8) // NaN
+
+//  // String
+//  const clientName = "Shunqor";
+//  console.log(clientName)
+//  // boolean
+//  const isMarried = false;
+//  const isCircleEarth = true;
+//  console.log(isMarried)
+//  console.log(isCircleEarth)
+
+//  // null
+// //  console.log(clienrAge)
+
+//  // undefinednot
+//  let und;
+//  console.log(und) 
+
+ // object
+// const thief = {
+// //  key: value
+//     age: 30, //number
+//     Jacket: "black", //string
+//     isLong: false, //boolean
+// };
+
+// console.log(thief.age);
+
+// // array    
+// //                1       2        3      4
+// const colors = ["red", "black", "green", 10]
+// console.log(colors[1])
+// const place = confirm("are you here ? ");
+// console.log(place);
+
+// const currentPlace = +prompt("where are you ?", "") 
+// console.log( currentPlace)
+
+// const favouriteColor = []
+
+// favouriteColor[0] = prompt("what's your favourite color ? #1", "")
+// favouriteColor[1] = prompt("what's your favourite color ? #2", "")
+// favouriteColor[2] = prompt("what's your favourite color ? #3", "")
+
+// console.log(favouriteColor)
+// const channel = prompt("what's your favourite you tuber")
+// console.log("https://youtube.com/" + channel + "/videos ")
+// const channel = prompt("what's your favourite youtuber")
+// console.log(`https://youtube.com/${channel}`)
+// const user = "Shunqor"
+// const job = "JavaScript developer"
+
+// console.log(`user name is${user}, heis${job}`)
+// let incr = 10;
+// let decr = 10;
+
+// incr++;  
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(5 % 2);
+// console.log(5 * 5 == 25 );
+
+// && = va; // || = yoki
+
+// const isAge = true;
+// const isClose = false;
+// const isChecked = true;
+// console.log(isAge && isClose && isChecked);
+// console.log( !isClose)
